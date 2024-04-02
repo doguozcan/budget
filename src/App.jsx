8f01bda36a03b1832f5b87e71a70ee630a4404cb
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-04-02 12:52:10 +0300

    Implement context api

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,26 @@ import Expenses from './components/Expenses'
 import Incomes from './components/Incomes'
 import AddExpense from './components/AddExpense.jsx'
 import AddIncome from './components/AddIncome'
+import { AppProvider } from './context/AppContext.jsx'
 
 export default function App() {
   return (
-    <div className="flex flex-col items-center font-montserrat">
-      <div className="flex space-x-2 m-4 w-4/5 justify-around">
-        <Total />
-        <Spent />
-        <Remaining />
+    <AppProvider>
+      <div className="flex flex-col items-center font-montserrat">
+        <div className="flex space-x-2 m-4 w-4/5 justify-around">
+          <Total />
+          <Spent />
+          <Remaining />
+        </div>
+        <div className="w-4/5 p-2">
+          <Expenses />
+          <Incomes />
+        </div>
+        <div className="w-4/5 ">
+          <AddIncome />
+          <AddExpense />
+        </div>
       </div>
-      <div className="w-4/5 p-2">
-        <Expenses />
-        <Incomes />
-      </div>
-      <div className="w-4/5 ">
-        <AddIncome />
-        <AddExpense />
-      </div>
-    </div>
+    </AppProvider>
   )
 }
---
+++ src/context/AppContext.jsx
@@ -0,0 +1,71 @@
+import { createContext, useReducer } from 'react'
+
+const initialState = {
+  total: 1024,
+  incomes: [
+    { id: 1, name: 'Rent', amount: 100 },
+    { id: 2, name: 'Job', amount: 1000 },
+  ],
+  expenses: [
+    { id: 1, name: 'Food', amount: 8 },
+    { id: 2, name: 'Travel', amount: 42 },
+    { id: 3, name: 'Cloth', amount: 128 },
+  ],
+}
+
+export const AppContext = createContext()
+
+function AppReducer(state, action) {
+  switch (action.type) {
+    case 'ADD_INCOME':
+      return {
+        ...state,
+        incomes: [...state.incomes, action.payload],
+        total: state.total + action.payload.amount,
+      }
+    case 'REMOVE_INCOME':
+      const updatedIncomes = state.incomes.filter(
+        (income) => income.id !== action.payload
+      )
+      const removedIncome = state.incomes.find(
+        (income) => income.id === action.payload
+      )
+
+      return {
+        ...state,
+        incomes: updatedIncomes,
+        total: state.total - removedIncome ? removedIncome.amount : 0,
+      }
+    case 'ADD_EXPENSE':
+      return {
+        ...state,
+        expenses: [...state.expenses, action.payload],
+        total: state.total - action.payload.amount,
+      }
+    case 'REMOVE_EXPENSE':
+      const updatedExpense = state.expenses.filter(
+        (expense) => expense.id !== action.payload
+      )
+      const removedExpense = state.expenses.find(
+        (expense) => expense.id === action.payload
+      )
+
+      return {
+        ...state,
+        expenses: updatedExpense,
+        total: state.total - removedExpense ? removedExpense.amount : 0,
+      }
+    default:
+      return state
+  }
+}
+
+export const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState)
+
+  return (
+    <AppContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AppContext.Provider>
+  )
+}
