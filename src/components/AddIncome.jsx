13a2d2da782e304cb7d17a1faf2a1c7651c613e3
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-04-03 08:36:21 +0300

    Implement add income functionality

--- src/components/AddIncome.jsx
+++ src/components/AddIncome.jsx
@@ -1,12 +1,47 @@
+import { useState } from 'react'
+import { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
+import { v4 as uuidv4 } from 'uuid'
+
 const AddIncome = () => {
+  const { dispatch } = useContext(AppContext)
+
+  const [incomeName, setIncomeName] = useState('')
+  const [incomeAmount, setIncomeAmount] = useState(0)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    dispatch({
+      type: 'ADD_INCOME',
+      payload: {
+        id: uuidv4(),
+        name: incomeName,
+        amount: incomeAmount,
+      },
+    })
+
+    setIncomeAmount('')
+    setIncomeName('')
+  }
+
   return (
-    <form className="flex flex-col gap-2">
+    <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
       <label>Income</label>
       <div className="flex justify-between gap-2">
         <input
           type="text"
-          placeholder="Add income..."
+          placeholder="Income..."
           className="input input-bordered input-success w-full"
+          value={incomeName}
+          onChange={(e) => setIncomeName(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Amount..."
+          className="input input-bordered input-success w-full"
+          value={incomeAmount}
+          onChange={(e) => setIncomeAmount(e.target.value)}
         />
         <button className="btn btn-outline btn-success">Add</button>
       </div>
